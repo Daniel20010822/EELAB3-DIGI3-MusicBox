686bec5c2ed1b4adb9f37ec74d37ef010850d15d
Daniel Wu <34362434+Daniel20010822@users.noreply.github.com>
2021-11-15 18:01:44 +0800

    新增文字檔錯誤行數標列功能

--- MusicSheetConverter.py
+++ MusicSheetConverter.py
@@ -1,16 +1,16 @@
 import_filename1 = "sheet1"
 import_filename2 = "sheet2"
-export_filename  = "MySong"
+export_filename  = "song"
+number_of_notes = 256
 
 # ----------------------------------------
-number_of_notes = 256
 import_file1 = import_filename1 + ".txt"
 import_file2 = import_filename2 + ".txt"
 export_file  = export_filename  + ".mif"
 
 # Pitch table
 pitch = {
-    "p" : "00000",
+    "p"    : "00000",
     "so<"  : "00001",
     "so+<" : "00010",
     "la<"  : "00011",
@@ -58,18 +58,47 @@ length = {
 }
 
 
-try:
-    with open(import_file1, "r") as sheet1, open(import_file2, "r") as sheet2:
-        notes1 = []
-        notes2 = []
-        for note in sheet1.readlines():
-            if note == "\n" or "#" in note: continue
-            else: notes1.append(length[note[0:2]] + pitch[note[2:-1]])
-        for note in sheet2.readlines():
-            if note == "\n" or "#" in note: continue
-            else: notes2.append(length[note[0:2]] + pitch[note[2:-1]])
+notes1 = []
+current_line1 = 1
+error_line1 = []
 
+notes2 = []
+current_line2 = 1
+error_line2 = []
+
+try:
+    with open(import_file1, "r") as sheet1:
+        for note in sheet1.readlines():
+            if note == "\n" or "#" in note:
+                current_line1 += 1
+            elif note[0:2] not in length or note[2:-1] not in pitch:
+                error_line1.append(current_line1)
+                current_line1 += 1
+            else:
+                notes1.append(length[note[0:2]] + pitch[note[2:-1]])
+                current_line1 += 1
+except FileNotFoundError:
+    print(f"無法找到{import_file1}，請確定第1行檔名")
+
+try:
+    with open(import_file2, "r") as sheet2:
+        for note in sheet2.readlines():
+            if note == "\n" or "#" in note:
+                current_line2 += 1
+            elif note[0:2] not in length or note[2:-1] not in pitch:
+                error_line2.append(current_line2)
+                current_line2 += 1
+            else:
+                notes2.append(length[note[0:2]] + pitch[note[2:-1]])
+                current_line2 += 1
+except FileNotFoundError:
+    print(f"無法找到{import_file2}，請確定第2行檔名")
+
+
+
+if not error_line1 or not error_line2:
     with open(export_file, mode="w") as song:
+
         # Informations
         song.write("WIDTH=8;\n")
         song.write(f"DEPTH={number_of_notes};\n")
@@ -93,11 +122,11 @@ try:
             song.write("END;")
         else:
             song.write("END;")
-
-except FileNotFoundError:
-    print(f"無法找到{import_file1}或{import_file2}，請確定第2、3行是否與檔名相符")
-except KeyError:
-    print("讀取樂譜時發生錯誤！")
+else:
+    print(f"請檢察{import_file1}的以下行數：")
+    print(*error_line1, sep='\n')
+    print(f"請檢察{import_file2}的以下行數：")
+    print(*error_line2, sep='\n')
     print("請確定樂譜裡：")
     print("1. 音符是否輸入正確(對照音高與音長對照表)")
     print("2. 只有換行符號(or Enter鍵)或含 # 的留言")
